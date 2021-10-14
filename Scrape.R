cf9f322abe7c6f2db586f855f2cc5d6a79f3e9d0
JeanLuc-Oudshoorn <jeanluc@oudshoorn.pro>
2021-10-14 19:31:24 +0200

    Script now also scrapes the time index of the series. These are subsequently converted to date class elements which are used to create an xts object. Additionally log returns and percentual returns for the time series are calculated.

--- Scrape.R
+++ Scrape.R
@@ -1,6 +1,8 @@
 library(rvest)
 library(dplyr)
 library(stringr)
+library(lubridate)
+library(xts)
 
 
 link <- read_html("https://www.indexmundi.com/commodities/?commodity=aluminum&months=360")
@@ -15,3 +17,16 @@ aluv <- txt %>%
   str_remove_all(",") %>%
   as.numeric()
   
+indx <- txt %>%
+  str_extract_all("[A-Z][a-z]{2} [0-9]{4}") %>%
+  unlist() %>%
+  str_to_lower() %>%
+  parse_date_time(order = "b Y")
+
+dat <- xts(aluv, order.by = indx)
+
+colnames(dat) <- "price"
+
+dat$logreturn <- diff(log(dat$price))
+
+dat$perctreturn <- (exp(dat$logreturn)-1)*100
