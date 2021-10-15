c36dabd0f4963acfc1b82003f8b4ee3c04e2b8a9
JeanLuc-Oudshoorn <jeanluc@oudshoorn.pro>
2021-10-15 20:06:46 +0200

    Adapted the exploratory data anlysis to work with gold data.

--- EDA.R
+++ EDA.R
@@ -6,10 +6,10 @@ ddat <- as.data.frame(dat)
 
 
 
-plot(dat$price, main = "Aluminium Monthly Price - US Dollars per Metric Ton", ylab = "Price in USD", col = "darkred")
-plot(dat$diff, main = "First Order Difference Aluminium Price", col = "darkred")
-plot(dat$diff2, main = "Second Order Difference Aluminium Price", col = "darkred")
-hist(dat$perctreturn, breaks = 40, main = "Histogram of Percentual Returns", col = "darkred", xlab = "Monthly Return (%)")
-boxplot(ddat$perctreturn, horizontal = TRUE, main = "Boxplot of Monthly Returns", xlab = "Monthly Return (%)", col = "darkred")
+plot(dat$price, main = "Gold Monthly Price - US Dollars per Troy Ounce", ylab = "Price in USD", col = "goldenrod")
+plot(dat$diff, main = "First Order Difference Gold Price", col = "goldenrod")
+plot(dat$diff2, main = "Second Order Difference Gold Price", col = "goldenrod")
+hist(dat$perctreturn, breaks = 40, main = "Histogram of Percentual Returns", col = "goldenrod", xlab = "Monthly Return (%)")
+boxplot(ddat$perctreturn, horizontal = TRUE, main = "Boxplot of Monthly Returns", xlab = "Monthly Return (%)", col = "goldenrod")
 
 (exp(median(na.omit(dat$logreturn)))-1)*100
